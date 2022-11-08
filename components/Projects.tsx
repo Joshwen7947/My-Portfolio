37cbee1a3f040dfe2d356a7ba4a52f7b8146e9cf
Josh <joshuawen97@gmail.com>
2022-11-08 15:16:16 +0700

    final before contact page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,14 +5,23 @@ type Props = {};
 function Projects({}: Props) {
 	const projects = [1, 2, 3, 4, 5];
 	return (
-		<div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+		<motion.div
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			transition={{ duration: 1.5 }}
+			className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+		>
 			<h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
 				Projects
 			</h3>
 			<div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
 				{projects.map((project, i) => (
 					<div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-						<img
+						<motion.img
+							initial={{ y: -300 }}
+							transition={{ duration: 1.2 }}
+							whileInView={{ opacity: 1, y: 0 }}
+							viewport={{ once: true }}
 							src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png"
 							alt=""
 						/>
@@ -34,7 +43,7 @@ function Projects({}: Props) {
 				))}
 			</div>
 			<div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12" />
-		</div>
+		</motion.div>
 	);
 }
 
